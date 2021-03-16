9d7ea7d8b62b04b1a7f7c9b681e76fb07b25368c
Sarasr <sara.kacem@etu.u-paris.fr>
2021-03-16 11:02:26 +0000

    Rendu Tp

---
+++ main.py
@@ -0,0 +1,18 @@
+note1 = ('eleve1', 'math', 13)
+note2 = ('eleve1', 'physique', 15)
+note3 = ('eleve1', 'math', 13)
+note4 = ('eleve1', 'eco', 12)
+note5 = ('eleve1', 'eco', 13)
+note6 = ('eleve1', 'math', 12)
+note7 = ('eleve2', 'math', 13)
+note8 = ('eleve2', 'math', 14)
+
+notes = [note1, note2, note3, note4, note5, note6,note7,note8]
+
+def moyenne(x,y):
+  return (x[2]+y[2])/2
+
+eleve1=note1+note2
+#+note3+note3+note4+note5+note6
+#print(moyenne(note1,note2))
+print(notes)
